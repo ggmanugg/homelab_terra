e3d97003d54dc7b2dd0150ca8222160401590d1c
Manuel Berger <manuel.berger00@gmail.com>
2025-08-31 20:55:35 +0200

    fix locals compose file path

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  compose_files = fileset(path.module, "*/docker-compose.yml")
+  compose_files = fileset("${path.module}/../stacks", "*/compose.yml")
   stacks = {
     for f in local.compose_files :
     split("/", f)[0] => f
